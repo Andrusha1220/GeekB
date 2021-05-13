c02a01007f220718c04a9e9f7d74ec17e7b3eb1b
Andrey <1220aav@gmail.com>
2021-05-13 16:09:47 +0300

    Lesson-3-homework

---
+++ Lesson3/Lesson3/Complex.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Lesson3
+{
+    public class Complex
+    {
+        private double im;
+        private double re;
+
+        public Complex(double im, double re)
+        {
+            this.im = im;
+            this.re = re;
+        }
+
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                if (value == 0)
+                    throw new Exception("Can't assign 0");
+                im = value;
+            }
+        }
+
+        public double Re
+        {
+            get { return re; }
+            set { re = value; }
+        }
+
+        public Complex Add(Complex x)
+        {
+            Complex y = new Complex(this.im + x.im, this.re + x.re);
+            return y;
+        }
+
+        public Complex Subtract(Complex x)
+        {
+            Complex y = new Complex(this.im - x.im, this.re - x.re);
+            return y;
+        }
+
+        public Complex Multiply(Complex x)
+        {
+            Complex y = new Complex(this.im * x.im, this.re * x.re);
+            return y;
+        }
+
+        public override string ToString()
+        {
+            return $"{re} + {im}i";
+        }
+    }
+}
